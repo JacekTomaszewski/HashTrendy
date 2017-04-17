f25f53d9892ccb490e0471db0bb1b1154fa12bde
JacekTomaszewski <Jacektomaszewski1@gmail.com>
2017-04-18 00:38:40 +0200

    Dodanie projektu WebService w VisualStudio

---
+++ webService/WebApiHash/WebApiHash/Context/HashContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApiHash.Models;
+
+namespace WebApiHash.Context
+{
+    public class HashContext: DbContext
+    {
+        public DbSet<Hashtag> Hashtags { get; set; }
+        public DbSet<Device> Devices { get; set; }
+    }
+}
---
+++ webService/WebApiHash/WebApiHash/Controllers/HashController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApiHash.Context;
+using WebApiHash.Models;
+
+namespace WebApiHash.Controllers
+{
+    public class HashController : Controller
+    {
+        HashContext db = new HashContext();
+
+        public object DeviceId { get; private set; }
+
+        public ActionResult Index()
+        {
+
+            return View(db.Devices.ToList());
+        }
+        public ActionResult Create()
+        {
+            String UniqueIdOfDeviceToCreate = "35444444440000400";
+            ViewBag.UniqueIdOfDeviceToCreate = UniqueIdOfDeviceToCreate;
+            db.Database.ExecuteSqlCommand("INSERT INTO Devices (DeviceUniqueId) VALUES (" + UniqueIdOfDeviceToCreate +")", DeviceId);
+            return View();
+        }
+
+        public ActionResult DeleteImei()
+        {
+            int IdOfDeviceToDelete = 6;
+            db.Database.ExecuteSqlCommand("Delete from Devices where DeviceId = "+IdOfDeviceToDelete, DeviceId);
+            return View(db.Devices.ToList());
+        }
+
+    }
+}
