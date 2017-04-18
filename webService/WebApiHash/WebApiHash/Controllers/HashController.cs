e1cd83ba492fc9e49b0a0384fb39ad033f5c019f
kanline1 <kamilhoppe95@gmail.com>
2017-04-18 17:16:26 +0200

    Poprawka
    Dodanie usuwania rekordu i poprawka do dodawania rekordu z URL przyklad
    : localhost:51577/hash/create?imei=1231231221

--- webService/WebApiHash/WebApiHash/Controllers/HashController.cs
+++ webService/WebApiHash/WebApiHash/Controllers/HashController.cs
@@ -21,20 +21,30 @@ namespace WebApiHash.Controllers
 
             return View(db.Devices.ToList());
         }
-        public ActionResult Create()
-        {
-            String UniqueIdOfDeviceToCreate = "35444444440000400";
-            ViewBag.UniqueIdOfDeviceToCreate = UniqueIdOfDeviceToCreate;
-            db.Database.ExecuteSqlCommand("INSERT INTO Devices (DeviceUniqueId) VALUES (" + UniqueIdOfDeviceToCreate +")", DeviceId);
+        public ActionResult Create(String imei)
+        {    
+            ViewBag.imei = imei;
+            db.Database.ExecuteSqlCommand("INSERT INTO Devices (DeviceUniqueId) VALUES ('" + imei + "')", DeviceId);
             return View();
         }
 
-        public ActionResult DeleteImei()
+        public ActionResult Delete(int id = 0)
         {
-            int IdOfDeviceToDelete = 6;
-            db.Database.ExecuteSqlCommand("Delete from Devices where DeviceId = "+IdOfDeviceToDelete, DeviceId);
-            return View(db.Devices.ToList());
+            Device DeviceId = db.Devices.Find(id);
+            if (DeviceId == null)
+            {
+                return HttpNotFound();
+            }
+            return View(DeviceId);
         }
-
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id = 0)
+        {
+            Device DeviceId = db.Devices.Find(id);
+            db.Devices.Remove(DeviceId);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    
     }
 }
