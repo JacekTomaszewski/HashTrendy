48585e412dc35972e57cec6bbcdef501295e74a9
DamianJankowiak <dd.jankowiak@gmail.com>
2017-04-25 00:19:32 +0200

    Dostęp po ip

---
+++ WebApiHash2/WebApiHash/Models/Device.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiHash.Models
+{
+    public class Device
+    {
+        public int DeviceId { get; set; }
+        public string DeviceUniqueId { get; set; }
+        public virtual ICollection<Hashtag> Hashtags { get; set; }
+
+    }
+}
