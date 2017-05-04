5cbd75f9efd45f3c9061a95b343946143426c4ca
JacekTomaszewski <Jacektomaszewski1@gmail.com>
2017-05-05 01:13:43 +0200

    Added new functionality
    Added new Packages (Linq,TweetSharp)
    Added partial support for Instagram (Authorization for user and
    JSON-view of his informations)
    Added partial support for Twitter (Authorization for user, searching and
    viewing first status with #cr7 hashtag)
    
    TO DO:
    -View all hashtags with #cr7
    (Object news - title, link unique, timestamp)

--- webService/WebApiHash/WebApiHash/Controllers/HashController.cs
+++ webService/WebApiHash/WebApiHash/Controllers/HashController.cs
@@ -1,31 +1,103 @@
-﻿using System;
+﻿using Hammock.Serialization;
+using LinqToTwitter;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
+using TweetSharp;
 using WebApiHash.Context;
 using WebApiHash.Models;
 
 namespace WebApiHash.Controllers
 {
+
     public class HashController : Controller
     {
         HashContext db = new HashContext();
 
         public object DeviceId { get; private set; }
 
+
+
         public ActionResult Index()
         {
 
             return View(db.Devices.ToList());
         }
+
+        public ActionResult Twitterli()
+        {
+            List<TwitterStatus> listTwitterStatusRet = new List<TwitterStatus>();
+            var service = new TwitterService("O5YRKrovfS42vADDPv8NdC4ZS", "tDrCy3YypKhnIOBm0qgCipwGjoJVf7akHV6srkHnLHJm62WvMF");
+            service.AuthenticateWith("859793491941093376-kqRIYWY9bWyS10ATfqAVdwk1ZaxloEJ", "hbOXipioFNcyOUyWbGdVAXvoVquETMl57AZUTcbMh3WRv");
+            var twitterSearchResult = service.Search(new SearchOptions { Q = "#cr7" });
+
+            if (twitterSearchResult != null)
+            {
+                listTwitterStatusRet = ((List<TwitterStatus>)twitterSearchResult.Statuses);
+                foreach (TwitterStatus objTwitterStatus in listTwitterStatusRet)
+                {
+                    objTwitterStatus.CreatedDate = objTwitterStatus.CreatedDate.AddHours(-4);
+                }
+            }
+            ViewData["MyList"] = listTwitterStatusRet[0].Text;
+            return View();
+        }
+        public ActionResult TwitterAuth()
+        {
+
+            string Key = "O5YRKrovfS42vADDPv8NdC4ZS";
+            string Secret = "tDrCy3YypKhnIOBm0qgCipwGjoJVf7akHV6srkHnLHJm62WvMF";
+
+            TwitterService service = new TwitterService(Key, Secret);
+
+            OAuthRequestToken requestToken = service.GetRequestToken("http://localhost:51577/Hash/TwitterCallback");
+            
+            Uri uri = service.GetAuthenticationUrl(requestToken);
+
+            return Redirect(uri.ToString());
+        }
+
+        public ActionResult TwitterCallback(string oauth_token, string oauth_verifier)
+        {
+            var requestToken = new OAuthRequestToken { Token = oauth_token };
+
+            string Key = "O5YRKrovfS42vADDPv8NdC4ZS";
+            string Secret = "tDrCy3YypKhnIOBm0qgCipwGjoJVf7akHV6srkHnLHJm62WvMF";
+
+           
+                TwitterService service = new TwitterService(Key, Secret);
+
+                OAuthAccessToken accessToken = service.GetAccessToken(requestToken, oauth_verifier);
+
+                service.AuthenticateWith(accessToken.Token, accessToken.TokenSecret);
+
+                VerifyCredentialsOptions option = new VerifyCredentialsOptions();
+
+                TwitterUser user = service.VerifyCredentials(option);
+                TempData["Name"] = user.Name;
+                TempData["Userpic"] = user.ProfileImageUrl;
+                TempData["Status"] = user;
+            TempData["access"] = accessToken.Token;
+            TempData["access secret"] = accessToken.TokenSecret;
+            return View(TempData);
+
+        }
+
+
+
         public ActionResult Create(String imei)
-        {    
+        {
             ViewBag.imei = imei;
             //db.Database.ExecuteSqlCommand("INSERT INTO Devices (DeviceUniqueId) VALUES ('" + imei + "') ON DUPLICATE KEY UPDATE 'DeviceUniqueId' = 'DeviceUniqueId'", DeviceId);
-            db.Database.ExecuteSqlCommand("UPDATE Devices SET DeviceUniqueId='"+imei+"' WHERE DeviceUniqueId='"+imei+"' IF @@ROWCOUNT=0 INSERT INTO Devices (DeviceUniqueId) VALUES('"+imei+"')",DeviceId);
+            db.Database.ExecuteSqlCommand("UPDATE Devices SET DeviceUniqueId='" + imei + "' WHERE DeviceUniqueId='" + imei + "' IF @@ROWCOUNT=0 INSERT INTO Devices (DeviceUniqueId) VALUES('" + imei + "')", DeviceId);
             return View();
         }
 
@@ -46,6 +118,7 @@ namespace WebApiHash.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
-    
+
     }
+
 }
---
+++ webService/WebApiHash/WebApiHash/Controllers/InstagramCallback.aspx.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApiHash.Controllers
+{
+    public partial class InstagramCallback : System.Web.UI.Page
+    {
+        static String code = "";
+        InstagramRegister fromRegisterClass = new InstagramRegister();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+
+            if (!String.IsNullOrEmpty(Request["code"]) && !Page.IsPostBack)
+            {
+                code = Request["code"].ToString();
+                //  GetDataInstagramToken();
+                GetToken();
+            }
+        }
+
+        void GetToken()
+        {
+            var json = "";
+            try
+            {
+                NameValueCollection parameters = new NameValueCollection();
+                parameters.Add("client_id", fromRegisterClass.ClientId);
+                parameters.Add("client_secret", fromRegisterClass.ClientSecret);
+                parameters.Add("grant_type", "authorization_code");
+                parameters.Add("grant_type", "client_credentials"); 
+                parameters.Add("redirect_uri", fromRegisterClass.RedirectURI);
+                parameters.Add("code", code);
+
+                WebClient client = new WebClient();
+                var result = client.UploadValues("https://api.instagram.com/oauth/access_token", "POST", parameters);
+                var response = System.Text.Encoding.Default.GetString(result);
+                var jsResult = (JObject)JsonConvert.DeserializeObject(response);
+
+                Label.Text = "var response: " + response + "/n var jsResult: " + jsResult;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+
+            }
+        }
+    }
+}
